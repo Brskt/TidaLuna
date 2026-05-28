5019451e2a4f355e255056ed662bb6d636a6b3f8
Brskt <contact@brskt.be>
2026-05-28 10:34:15 +0200

    Manifest the updater as asInvoker
    Without it, Windows installer-detection elevates updater.exe (filename
    contains "update") and spawning fails with ERROR_ELEVATION_REQUIRED.

--- updater/build.rs
+++ updater/build.rs
@@ -3,6 +3,20 @@ fn main() {
     {
         let mut res = winres::WindowsResource::new();
         res.set_icon("../tidaluna.ico");
+        // asInvoker: without a manifest, Windows installer-detection elevates any "updater"-named exe (spawn fails 740).
+        res.set_manifest(
+            r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel level="asInvoker" uiAccess="false"/>
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+</assembly>
+"#,
+        );
         res.compile().expect("winres compile");
     }
 }
